f9a0bef1cf3f42efa4ddb5632328436d76d92eed
Deb Vorndran <deb@localtek.biz>
2018-04-17 17:28:30 -0400

    Passing Fxs as args === callback fxs

--- completeJavascript/5-Advanced-JS/script.js
+++ completeJavascript/5-Advanced-JS/script.js
@@ -1,3 +1,4 @@
+/*
 var Person = function(name, yearOfBirth, job){
 	this.name = name;
 	this.yearOfBirth = yearOfBirth;
@@ -26,3 +27,88 @@ susan.calcRetirementYear();
 //check all this stuff out in the console, by exploring mary instance
 
 //susan.lastName === "Smith"
+
+//Object.create  (instead of function contructors)
+var personProto = {
+	calculateAge: function(){
+		console.log(2018 - this.yearOfBirth);
+	}
+}
+
+var jon = Object.create(personProto);
+jon.name = 'jon';
+jon.yearOfBirth = '1990';
+jon.job = 'teacher';
+
+var jane = Object.create(personProto, {
+	name: { value: 'Jane'},
+	yearOfBirth: { value: 1969},
+	job: { value: 'hacker'},
+});
+
+// jon & jane are pretty much identical (except for the actual data)
+
+//Object.create allows us to create complicated stuff
+
+var a=23;
+var b=a;  //This is a Complete COPY of A
+a = 46;
+
+var obj1 = {
+	name: "John",
+	age: 26
+}
+
+var obj2 = obj1;
+obj1 = 30; //oops, but this was interesting
+var obj3 = obj2;
+obj2.age = 40;
+
+var age = 27;
+var objJonas = {
+	name: "Jonas",
+	city: "Lisban"
+	};
+
+function change( a, b){
+	//a is a prim, b is an obj
+	a = 31;
+	b.city = 'San Francisco';
+}
+
+change (age, objJonas);
+*/
+
+
+var years = [1990, 1965, 1937, 1945, 1999, 2005, 1904];
+
+//This is a callback Fx, el = element
+function maxHeartRate(el){
+	if( el >= 18 && el <= 81){
+		return Math.round(206.9 - (0.67 * el));
+	} else {
+		return -1;
+	}
+}
+//This is a callback Fx, el = element
+function isFullAge(el){
+	return el >= 18;
+}
+
+//This is a callback Fx, el = element
+function calcAge(el){
+	return 2018 - el;
+}
+
+function arrayCalc( arr, fn){
+	var arrRes = [];
+	for(var i=0; i < arr.length; i++){
+		arrRes.push(fn(arr[i]));
+	}
+	return arrRes;
+}
+
+// NOT calcAge():  which would execute it first
+var ages = arrayCalc( years, calcAge );
+var fullAges = arrayCalc( ages, isFullAge );
+var maxHRate = arrayCalc( ages, maxHeartRate );
